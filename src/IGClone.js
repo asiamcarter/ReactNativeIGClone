62f60eebcab931cdad7729fb320605c0ad33deee
Asia Carter <asiamcarter@gmail.com>
2019-05-28 15:09:00 -0500

    working on cropping image on the server side

--- src/IGClone.js
+++ src/IGClone.js
@@ -1,8 +1,22 @@
 import React, { Component } from 'react'
-import { View, Text, StyleSheet, Image } from 'react-native'
+import { View, Text, StyleSheet, Image, Dimensions } from 'react-native'
+
 
 class IGClone extends Component {
+     constructor(){
+         super();
+         this.state = {
+             screenWidth: Dimensions.get("window").width
+         };
+     }
+    // componentDidMount(){
+    //     this.setState({
+    //         screenWidth:
+    //     })
+    // }
     render(){
+        const imageUri = 'https://lh3.googleusercontent.com/dRE8jbTxbM-o9MxkrGR1rlJvQFB36yDU6EPr7zylncBqfdlDLuMgYMfw8Q7CfOWYSsY_27-2TbOSehadJUaXMw4WDqQ' + '=s' + Math.floor(this.state.screenWidth * 1.1) + "-c"
+
         return(
             <View style={{flex: 1, width: "100%", height: "100%" }}>
                 <View
@@ -12,20 +26,25 @@ class IGClone extends Component {
                 <View style={styles.userBar}>
                     <View style={{flexDirection:"row", alignItems: 'center'}}>
                         <Image
-                        style={{width: 40, height: 40}}
+                        style={styles.userImage}
                         source={{uri: 'https://lh3.googleusercontent.com/zGugq39hcKZ3kRyLRttBmZG6ssme0kxrYDYsp2u-vTTl2axUccwLEgeicistyXMboH_8ugxMlWpVeY7KokXhHpSE9Q'}}/>
-                        <Text>UserName</Text>
+                        <Text
+                        style={{
+                            marginLeft: 10
+                            }}>
+                                UserName
+                        </Text>
                     </View>
                     <View>
-
+                        <Text style={{fontSize: 22}}>...</Text>
                     </View>
                 </View>
 
 
                 <Image
-                style={{ width: "100%", height: 100 }}
+                style={{ width: this.state.screenWidth, height: 400}}
                 source={{
-                    uri: 'https://lh3.googleusercontent.com/dRE8jbTxbM-o9MxkrGR1rlJvQFB36yDU6EPr7zylncBqfdlDLuMgYMfw8Q7CfOWYSsY_27-2TbOSehadJUaXMw4WDqQ'}}
+                    uri: imageUri}}
                     />
             </View>
         )
@@ -46,7 +65,15 @@ const styles = StyleSheet.create({
         width: "100%",
         height: 50,
         backgroundColor: 'rgb(255, 255, 255)',
-        flexDirection: "row"
+        flexDirection: "row",
+        paddingHorizontal: 10,
+        alignItems: 'center',
+        justifyContent: 'space-between'
+    },
+    userImage: {
+        height: 40,
+        width: 40,
+        borderRadius: 20
     }
 });
 
