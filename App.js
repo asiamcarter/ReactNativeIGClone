b9a0c31046b49d5aa2f5522a6b7f2c39257aef68
Asia Carter <asiamcarter@gmail.com>
2019-05-28 14:33:29 -0500

    figuring out layout

--- App.js
+++ App.js
@@ -7,18 +7,9 @@ import IGClone from './src/IGClone.js'
 export default class App extends Component{
   render() {
     return (
-      <View style={styles.container}>
         <IGClone />
-      </View>
     );
   }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  }
-});
+
--- src/IGClone.js
+++ src/IGClone.js
@@ -4,11 +4,50 @@ import { View, Text, StyleSheet, Image } from 'react-native'
 class IGClone extends Component {
     render(){
         return(
-            <View>
-                <Text>My Instagram Clone</Text>
+            <View style={{flex: 1, width: "100%", height: "100%" }}>
+                <View
+                style={styles.tempNav}>
+                    <Text>Instagram</Text>
+                </View>
+                <View style={styles.userBar}>
+                    <View style={{flexDirection:"row", alignItems: 'center'}}>
+                        <Image
+                        style={{width: 40, height: 40}}
+                        source={{uri: 'https://lh3.googleusercontent.com/zGugq39hcKZ3kRyLRttBmZG6ssme0kxrYDYsp2u-vTTl2axUccwLEgeicistyXMboH_8ugxMlWpVeY7KokXhHpSE9Q'}}/>
+                        <Text>UserName</Text>
+                    </View>
+                    <View>
+
+                    </View>
+                </View>
+
+
+                <Image
+                style={{ width: "100%", height: 100 }}
+                source={{
+                    uri: 'https://lh3.googleusercontent.com/dRE8jbTxbM-o9MxkrGR1rlJvQFB36yDU6EPr7zylncBqfdlDLuMgYMfw8Q7CfOWYSsY_27-2TbOSehadJUaXMw4WDqQ'}}
+                    />
             </View>
         )
     }
 }
+const styles = StyleSheet.create({
+    tempNav: {
+        width: "100%",
+        height: 50,
+        marginTop: 20,
+        backgroundColor: 'rgb(250, 250, 250)',
+        borderBottomWidth: StyleSheet.hairlineWidth,
+        borderBottomColor: 'rgb(233, 233, 233)',
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+    userBar: {
+        width: "100%",
+        height: 50,
+        backgroundColor: 'rgb(255, 255, 255)',
+        flexDirection: "row"
+    }
+});
 
 export default IGClone;
